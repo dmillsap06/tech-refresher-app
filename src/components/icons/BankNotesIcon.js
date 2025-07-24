a3eec3c3a88f3f2166196131700aaa7f4d7b5cf5
Dillon Millsap <dillonmillsap1@gmail.com>
2025-07-24 12:38:26 -0400

    v1.4.2

--- src/components/icons/BankNotesIcon.js
+++ src/components/icons/BankNotesIcon.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+//comment
 const BanknotesIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <rect x="2" y="7" width="20" height="10" rx="2" fill="currentColor" opacity="0.1"/>
